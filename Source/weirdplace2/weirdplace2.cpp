f1cbc16729920623a8ea3d990912f153ae6d30f1
Ethan Fischer <ethan.f.fischer@gmail.com>
2024-06-23 14:25:12 -0500

    Level design gas station (#2)
    * Entry way colliders
    
    * Repair actor that got messed up after pulling master after lfs migration
    
    * move mist up
    
    * Glass fog lookin better
    
    * dunno why this doesnt save with ctrl s
    
    * clean up ivy a little
    
    * gas station in level
    
    * Gas station added
    
    * emmisive material on gastation
    
    * ceiling lights
    
    * the lighting looks so fuckin good
    
    * scaled down a bit
    
    * played with lighting some more
    
    * colliders in gas station partially
    
    * gas station colliders complete with one issue
    
    * downsize player collider a bit and make bathroom doorway a bit bigger
    
    * initial phase of redoing gas station going well
    
    * bathroom wall added
    
    * added some sensible first textures to gastation
    
    * gas station is more or less at parity enough now
    
    * more
    
    * block out gastation
    
    * minor adjustments to geo and delete old gasstation
    
    * connecting gasstation to weirdplace
    
    * secret tunnel under toilet
    
    * tunnel improvements
    
    * WIP innter tube coliiders
    
    * in a good place with connectirng tunnel
    
    * wip rampway to lower level
    
    * Fix killz being too high in world settings and try to compile something in rider (faileed)
    
    * Assigning materials
    
    * lighting and textures
    
    * Ground complete and materials added to exterior
    
    * Also add wind sounds
    
    * doorglass
    
    * some collision fixes and invisible boundaries added
    
    * idk

---
+++ Source/weirdplace2/weirdplace2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "weirdplace2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, weirdplace2, "weirdplace2" );
