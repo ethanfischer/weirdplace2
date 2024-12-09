cf4b18f2d06defd83ae0696bf47e72080b2864fd
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-12-09 20:44:59 +0800

    c++ setup stuff

---
+++ Source/weirdplace2/MovieBox.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovieBox.h"
+
+// Sets default values
+AMovieBox::AMovieBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovieBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovieBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
