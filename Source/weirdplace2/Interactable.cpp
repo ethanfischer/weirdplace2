4c4bf9d8dee84be1bb13dbff18667387a742c5bc
ethanfischer <ethan.f.fischer@gmail.com>
2025-03-13 18:43:53 -0500

    Interactable interace in cpp WIP

---
+++ Source/weirdplace2/Interactable.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable.h"
+
+
+// Add default functionality here for any IUInteractable functions that are not pure virtual.
+void IInteractable::InteractWithObject(UObject* Object)
+{
+}
